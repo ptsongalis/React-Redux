06c56a5d4cc84250bde0d92ba2b133b01c0c7fdc
Peter Tsongalis <hhgoal14@gmail.com>
2019-01-12 10:23:57 -0500

    init

---
+++ Project1/src/index.js
@@ -0,0 +1,37 @@
+//Import the React and ReactDOM Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a react component
+    //function or class
+        //produces html to show (using JSX)
+        //handles feedback from user (event handlers)
+
+// const App = function() {
+//     return <div> Hi there!</div>
+// };
+
+function getButtonText() {
+    return 'Click On Me';
+};
+
+function getLabelText() {
+    return 'Enter Name: ';
+}
+
+const App = () => {
+   
+    return (
+        <div>
+            <label className="label" htmlFor="name"> {getLabelText()} </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor : 'blue', color: 'white'}}> {getButtonText()} </button>
+        </div>
+    );
+}
+
+//take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
