b14e20eb74b00346bc09b39adc8f47d74ad20d34
Peter Tsongalis <hhgoal14@gmail.com>
2019-01-12 15:12:27 -0500

    began lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -10,20 +10,46 @@ class App extends React.Component
         super(props); //must call the super with props passed in
 
         //this is the only time we do direct assignment to this.state
-        this.state = { lat: null, long: null }; 
+        this.state = { lat: null, long: null, errorMessage: '' }; 
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                this.setState({lat: position.coords.latitude, long: position.coords.longitude});
+                this.setState(
+                    {lat: position.coords.latitude, 
+                        long: position.coords.longitude
+                    });
             },
-            (err) => console.log(err)
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
         );
     }
 
+    componentDidMount()
+    {
+        console.log('my component was rendered');
+    }
 
+    componentDidUpdate()
+    {
+        console.log('my component just updated');
+    }
+
+    //render is called once when the app starts and 
+    //every time the state is modified
     render() //we must define the render method
     {
-        return <div>Latitude: {this.state.lat} Longitude: {this.state.long}</div>
+            if (this.state.errorMessage && !this.state.lat)
+            {
+                return <div>Error: {this.state.errorMessage}</div>
+            }
+
+            if(!this.state.errorMessage && this.state.lat)
+            {
+                return <div>Latitude: {this.state.lat}</div>
+            }
+
+            return <div>Loading...</div>
     }
 
 }
