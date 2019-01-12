a1d9e394170d08c820b8be15559fb1c2a4017346
Peter Tsongalis <hhgoal14@gmail.com>
2019-01-12 14:46:25 -0500

    constructor and state created

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class App extends React.Component {
+class App extends React.Component 
+{
     
-    render()
+    constructor(props)
+    {
+        super(props); //must call the super with props passed in
+
+        this.state = { lat: null, long: null };
+    }
+
+
+    render() //we must define the render method
     {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
 
-        return <div>Latitude: </div>
+        return <div>Latitude:</div>
     }
 
 }
