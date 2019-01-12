0d95930bec3eec318b641f047eb6ff793c5a2e7a
Peter Tsongalis <hhgoal14@gmail.com>
2019-01-12 11:08:59 -0500

    create component for comment detail

--- components/src/index.js
+++ components/src/index.js
@@ -2,55 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import CommentDetail from './CommentDetail';
+
 const App = () => {
     return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/> 
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span> 
-                    </div>
-                    <div className="text">Nice Blog Post!</div>
-                </div>
-            </div>
-            <p/>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/> 
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Joe
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span> 
-                    </div>
-                    <div className="text">Nice Blog Post!</div>
-                </div>
-            </div>
-            <p/>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/> 
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        John
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span> 
-                    </div>
-                    <div className="text">Nice Blog Post!</div>
-                </div>
-            </div>
+       <div className="ui container comments">
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+       </div>
         
-        </div>
     );
 };
 
