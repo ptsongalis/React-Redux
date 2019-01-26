ca4e2a9d80f3b25824f82631fc58ccc2c42b2646
Peter Tsongalis <hhgoal14@gmail.com>
2019-01-26 10:34:33 -0500

    css for styling, semantic icons for snowflakes and suns

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,34 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        iconName: 'sun',
+        text: "Lets hit the beach!"  
+    },
+    winter: {
+        iconName: 'snowflake',
+        text: "Burr, it is cold!"  
+    }
+};
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'; //ternary expression
+    }
+    else
+        return lat > 0 ? 'winter' : 'summer';
+}
+
 const SeasonDisplay = (props) => {
-    
-    return <div>{props.lat}</div>;
+    const season = getSeason(props.lat, new Date().getMonth());
+    const{text, iconName} = seasonConfig[season] // {text, iconName}
+
+    return <div className = {`season-display ${season}`}>
+            <i className = {`icon-left massive ${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className = {`icon-right massive ${iconName} icon`}/>
+        </div>;
 };
 
 export default SeasonDisplay;
