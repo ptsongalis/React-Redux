6729b66995a66c6a65ecf7bc3a42c1653bb1331f
Peter Tsongalis <hhgoal14@gmail.com>
2019-01-26 15:06:10 -0500

    axios install and get request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 1cdaeb5e56c04fe4e8b77dd67ec88d9d2a5536a777e457a722653d755dbf53d0'
+            }
+        });
     }
 
     render() {
