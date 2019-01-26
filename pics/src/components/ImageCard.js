284a452ddfc40ff80bbdc25355050349f711daf7
Peter Tsongalis <hhgoal14@gmail.com>
2019-01-26 16:26:36 -0500

    image app finished

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -6,6 +6,7 @@ class ImageCard extends React.Component {
         super(props);
 
         this.imageRef = React.createRef();
+        this.state = { spans: 0 };
     }
 
     componentDidMount() {
@@ -13,14 +14,17 @@ class ImageCard extends React.Component {
     }
 
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10);
+        this.setState({ spans: spans });
     }
 
     render() {
         const { description, urls } = this.props.image;
 
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img
                     ref={this.imageRef}
                     alt={description}
